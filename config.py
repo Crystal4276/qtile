4766a857efce963eda4d7c54054322e57e388bb5
Crystal4276 <samy.ouldchikh@gmail.com>
2022-10-27 17:15:22 +0300

    various cleaning of qtile.py

--- config.py
+++ config.py
@@ -1,110 +1,20 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import os
 import subprocess
 
-from libqtile import bar, layout, hook
+from libqtile import bar, layout, hook, qtile
 from libqtile.log_utils import logger
-from qtile_extras import widget
-from qtile_extras.widget.decorations import RectDecoration, PowerLineDecoration
-
 from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen, Rule, DropDown
 from libqtile.lazy import lazy
 from libqtile.widget import Spacer
 
+from qtile_extras import widget
+from qtile_extras.widget.decorations import RectDecoration, PowerLineDecoration
 
+# Parsing : remove all text.
 def txt_remove(text):
     return ""
 
-@lazy.function
-def minimize_all(qtile):
-    for win in qtile.current_group.windows:
-        if hasattr(win, "toggle_minimize"):
-            win.toggle_minimize()
-
-mod = "mod4"
-
-keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    #Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    #Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    #Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    #Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "control"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "control"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "control"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "control"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "shift"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "shift"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "shift"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "shift"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    #Key(
-    #    [mod, "shift"],
-    #    "Return",
-    #    lazy.layout.toggle_split(),
-    #    desc="Toggle between split and unsplit sides of stack",
-    #),
-
-#Toggle minimization/fullscreen of appararent window
-    Key([mod],"c", minimize_all(), desc="Toggle minimization of all window"),
-    Key([mod],"f", lazy.window.toggle_fullscreen(), desc="Make window fullscreen"),
-
- # Launch Applications
-    Key([mod],"e", lazy.spawn("nemo"), desc="Launch nemo"),
-    Key([mod],"w", lazy.spawn("/home/crystal/.config/qtile/rofi/bin/launcher"), desc="Launch rofi"),
-    Key([mod],"x", lazy.spawn("geany"), desc="Launch geany editor"),
-    Key([mod],"a", lazy.spawn("chromium"), desc="Launch Chromium browser"),
-    Key([mod], "Return", lazy.spawn("gnome-terminal -e \"bash -c neofetch\";bash"), desc="Launch terminal"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([],"Print", lazy.spawn("gnome-screenshot --interactive"), desc="Launch screenshot"),
-
- # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-
- # Qtile commands
-    Key([mod, "mod1"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-]
-
-# Send a window within a group to group displayed in left or right screens, three monitors, monitor 0 is central monitor. 
+# Send a window within a group to group displayed in left or right screens. Three monitors configuration, monitor 0 is the central monitor -> Screens: [1,0,2] 
 def window_to_previous_screen(qtile, switch_group=False, switch_screen=False):
     i = qtile.screens.index(qtile.current_screen)
     if i == 0:
@@ -131,28 +41,11 @@ def window_to_next_screen(qtile, switch_group=False, switch_screen=False):
         if switch_screen == True:
             qtile.to_screen(i - 1)
 
-keys.extend([
-    # MOVE WINDOW TO NEXT SCREEN
-    Key([mod], "Right", lazy.function(window_to_next_screen, switch_screen=True)),
-    Key([mod], "Left", lazy.function(window_to_previous_screen, switch_screen=True)),
-])
-
-groups = [
-    Group("1", label=""),
-    Group("2", label=""),
-    Group("3", label=""), 
-    Group("4", label="", matches=[Match(wm_class=["discord"])],layout = "max"),
-    Group("5", label="", matches=[Match(wm_class=["spotify"])],layout = "max"),
-    Group("6", label=""),
-    Group("7", label="", matches=[Match(wm_class=["deluge"])],layout = "columns"),
-    Group("8", label="", matches=[Match(wm_class=["evolution","thunderbird"])],layout = "max"),
-    Group("9", label="", matches=[Match(wm_class=["Steam"])],layout = "max"),
-]
-#### Key binding for group stick to screen
+# Key bindings for group stick to screen
 def go_to_group(name: str) -> callable:
     def _inner(qtile) -> None:
         if len(qtile.screens) == 1:
-            qtile.groups_map[name].cmd_toscreen()
+            qtile.groups_map[name].toscreen()
             return
         if name in "123":
             qtile.focus_screen(0)
@@ -164,23 +57,6 @@ def go_to_group(name: str) -> callable:
             qtile.focus_screen(2)
             qtile.groups_map[name].toscreen()
     return _inner
-
-for i in groups:
-    keys.append(Key([mod], i.name, lazy.function(go_to_group(i.name))))
-
-for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-#            Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name),),
-
-            # mod1 + control + letter of group = switch to & move focused window to group and screen
-            Key([mod, "control"], i.name, lazy.window.togroup(i.name, switch_group=True), lazy.function(go_to_group(i.name)), desc="Switch to & move focused window to group {}".format(i.name),),
-
-            # Or, use below if you prefer not to switch to that group. mod1 + shift + letter of group = move focused window to group
-           #Key([mod, "shift"], i.name, lazy.window.togroup(i.name), desc="move focused window to group {}".format(i.name)),
-       ]
-    )
 
 # COLORS FOR THE BAR
 #Theme name : Catppuccin Mocha
@@ -196,6 +72,88 @@ def init_colors():
             ["#f3f4f500", "#f3f4f500"], # color 8 White 66 % Tranparency
             ["#11111b", "#11111b"]] # color 9 Crust
 colors = init_colors()
+
+@lazy.function
+def minimize_all(qtile):
+    for win in qtile.current_group.windows:
+        if hasattr(win, "toggle_minimize"):
+            win.toggle_minimize()
+
+mod = "mod4"
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    #Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
+    #Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
+    #Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
+    #Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
+    #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    
+    # Move windows between left/right columns or move up/down in current stack.
+    Key([mod, "control"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "control"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows.
+    Key([mod, "shift"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "shift"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "shift"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "shift"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    #Key(
+    #    [mod, "shift"],
+    #    "Return",
+    #    lazy.layout.toggle_split(),
+    #    desc="Toggle between split and unsplit sides of stack",
+    #),
+    # Move window to next screen
+    Key([mod], "Right", lazy.function(window_to_next_screen, switch_screen=True),desc="Move window to right screen"),
+    Key([mod], "Left", lazy.function(window_to_previous_screen, switch_screen=True), desc="Move window to left screen"),
+
+#Toggle minimization/fullscreen/kill of windows
+    Key([mod],"c", minimize_all(), desc="Toggle minimization of all window"),
+    Key([mod],"f", lazy.window.toggle_fullscreen(), desc="Make window fullscreen"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+
+ # Launch Applications
+    Key([mod],"e", lazy.spawn("nemo"), desc="Launch Nemo"),
+    Key([mod],"w", lazy.spawn("/home/crystal/.config/qtile/rofi/bin/launcher"), desc="Launch Rofi"),
+    Key([mod],"x", lazy.spawn("geany"), desc="Launch Geany editor"),
+    Key([mod],"a", lazy.spawn("chromium"), desc="Launch Chromium browser"),
+    Key([mod], "Return", lazy.spawn("gnome-terminal -e \"bash -c neofetch\";bash"), desc="Launch Terminal"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([],"Print", lazy.spawn("gnome-screenshot --interactive"), desc="Launch Screenshot"),
+
+ # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+ # Qtile commands
+    Key([mod, "mod1"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload Qtile config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+]
+
+groups = [
+    Group("1", label=""),
+    Group("2", label=""),
+    Group("3", label=""), 
+    Group("4", label="", matches=[Match(wm_class=["discord"])],layout = "max"),
+    Group("5", label="", matches=[Match(wm_class=["spotify"])],layout = "max"),
+    Group("6", label=""),
+    Group("7", label="", matches=[Match(wm_class=["deluge"])],layout = "columns"),
+    Group("8", label="", matches=[Match(wm_class=["evolution","thunderbird"])],layout = "max"),
+    Group("9", label="", matches=[Match(wm_class=["Steam"])],layout = "max"),
+]
+
+for i in groups:
+    keys.append(Key([mod], i.name, lazy.function(go_to_group(i.name))))
+    keys.append(Key([mod, "control"], i.name, lazy.window.togroup(i.name, switch_group=True), lazy.function(go_to_group(i.name)), desc="Switch to & move focused window to group {}".format(i.name)))
 
 # Decoration setting for systray Rect.Decoraction
 decor_systray = {
@@ -324,28 +282,6 @@ decor_side = {
         )
     ],
 }
-
-# Layout configuration
-layout_theme = {"border_width": 1,
-                "margin": 4,
-                "border_focus": colors[2],
-                "border_normal": colors[6]
-                }
-layouts = [
-    layout.Columns(**layout_theme),
-    # layout.Matrix(**layout_theme),
-    #layout.RatioTile(**layout_theme),
-    layout.Max(**layout_theme),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.MonadTall(),
-    # layout.MonadWide(**layout_theme),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
-]
 
 widget_defaults = dict(
     font="monospace",
@@ -630,6 +566,28 @@ screens = [
     ),   
 ]
 
+# Layout configuration
+layout_theme = {"border_width": 1,
+                "margin": 4,
+                "border_focus": colors[2],
+                "border_normal": colors[6]
+                }
+layouts = [
+    layout.Columns(**layout_theme),
+    # layout.Matrix(**layout_theme),
+    #layout.RatioTile(**layout_theme),
+    layout.Max(**layout_theme),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.MonadTall(),
+    # layout.MonadWide(**layout_theme),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
 # Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
@@ -665,14 +623,6 @@ floating_layout = layout.Floating(
     ], fullscreen_border_width = 0, border_width = 2, border_focus=colors[2], border_normal=colors[6]
 )
 
-#layout_conky = layout.Floating(
-#    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-#        *layout.Floating.default_float_rules,
-#        Match(wm_class="conky"),  # conky
-#    ], border_focus=colors[4], border_normal=colors[4]
-#)
-
 auto_fullscreen = True
 focus_on_window_activation = "smart" #or focus
 reconfigure_screens = True
@@ -684,14 +634,7 @@ auto_minimize = True
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
+# Window Manager Name 
 wmname = "Qtile"
 
 # If Spotify opens move it to proper group
@@ -715,13 +658,33 @@ def window_added(group, window):
 def autostart():
     home = os.path.expanduser('~/.config/qtile/autostart.sh')
     subprocess.Popen([home])
-    
+
+# Pass every Steam window that is not the main one in floating mode
+@hook.subscribe.client_new
+def float_steam(window):
+    wm_class = window.window.get_wm_class()
+    w_name = window.window.get_name()
+    if (
+        wm_class == ("Steam", "Steam")
+        and (
+            w_name != "Steam"
+            # w_name == "Friends List"
+            # or w_name == "Screenshot Uploader"
+            # or w_name.startswith("Steam - News")
+            or "PMaxSize" in window.window.get_wm_normal_hints().get("flags", ())
+        )
+    ):
+        window.floating = True
+
 # Activate group 6, 7, and 1 after startup
 @hook.subscribe.startup_complete
 def assign_groups_to_screens():
-    try:
-        qtile_global.groups_map["1"].cmd_toscreen(0)
-        qtile_global.groups_map["6"].cmd_toscreen(1)
-        qtile_global.groups_map["7"].cmd_toscreen(2)
-    except IndexError:
-        pass
+	if len(qtile.screens) != 1:
+		try:
+			qtile.groups_map["1"].toscreen(0)
+			qtile.groups_map["6"].toscreen(1)
+			qtile.groups_map["8"].toscreen(2)
+		except IndexError:
+			pass
+
+
